f467a12311d859eb0de6c3623c05834ff92e247f
crganzon@gmail.com <cezarganzon@Cezars-MacBook-Pro.local>
2019-03-04 14:01:43 -0800

    accessing html inputs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,20 @@
 // Initial variables 
 var queryUrl = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=GOPCdtWayhrv5ArqA9m1VGtG2Mj7UH7T"; // add search terms, number, and years
-var searchTerm
-var articleMinYear 
-var articleMaxYear
+var searchTerm = $("#searchInput1").val();
+var articleNumber = $("#numberOfRecords1").val();
+var minYear = $("#startYear1").val();
+var maxYear = $("#endYear1").val();
 
-// Ajax function to access API
-<<<<<<< HEAD
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
+$("#searchBtn").on("click", function () {
+    console.log(searchTerm);
+    console.log(minYear);
+    console.log(maxYear);
+    // Ajax function to access API
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
 
-});
-=======
-$.ajax
-
-
-
-$("#searchBtn").click(function(){
-
-});
-
-
-$("#clearBtn").click(function(){
-
-});
-
->>>>>>> 351f04e0973007e2b7edb6d3736ce9e13bda81ea
+    });
+})
