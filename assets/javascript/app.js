6af904e2aad73c0a9c70f2013afc3553bb1cbbd5
crganzon@gmail.com <cezarganzon@Cezars-MacBook-Pro.local>
2019-03-04 13:30:56 -0800

    making ajax function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,14 @@
-// Initial variables 
-var queryUrl = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=GOPCdtWayhrv5ArqA9m1VGtG2Mj7UH7T";
+alert("hello");
+// // Initial variables 
+var queryUrl = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=GOPCdtWayhrv5ArqA9m1VGtG2Mj7UH7T"; // add search terms, number, and years
 var searchTerm = ;
 var articleNumbers = ;
 
-// Ajax function to access API
-$.ajax
+// // Ajax function to access API
+$.ajax({
+    url: queryUrl,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+
+});
