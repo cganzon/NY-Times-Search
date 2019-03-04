9a0626876a2f839623e08c3ffbaec6d5db32a732
crganzon@gmail.com <cezarganzon@Cezars-MacBook-Pro.local>
2019-03-04 13:20:02 -0800

    moved js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
 // Initial variables 
-var queryUrl = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=GOPCdtWayhrv5ArqA9m1VGtG2Mj7UH7T"
+var queryUrl = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=GOPCdtWayhrv5ArqA9m1VGtG2Mj7UH7T";
+var searchTerm = ;
+var articleNumbers = ;
+
 // Ajax function to access API
 $.ajax
--- javascript/app.js
+++
@@ -1,7 +0,0 @@
-// Initial variables 
-var queryUrl = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=GOPCdtWayhrv5ArqA9m1VGtG2Mj7UH7T";
-var searchTerm = ;
-var articleNumbers = ;
-
-// Ajax function to access API
-$.ajax
